5953546715847034ea57bc110ba303ed70859fcd
Oluwafemi <dapriest01@gmail.com>
2020-05-02 21:10:01 +0100

    Updates

---
+++ precache-manifest.e03104b0ee2662e76ca1dea2f673f6ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22392ad5ec7982720bbc6196b102d52a",
+    "url": "/Robo/index.html"
+  },
+  {
+    "revision": "230010cf6f839d83c729",
+    "url": "/Robo/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "83e2cf6d3553fe937534",
+    "url": "/Robo/static/css/main.8fd0617f.chunk.css"
+  },
+  {
+    "revision": "230010cf6f839d83c729",
+    "url": "/Robo/static/js/2.367ab4d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo/static/js/2.367ab4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e2cf6d3553fe937534",
+    "url": "/Robo/static/js/main.35c54bf0.chunk.js"
+  },
+  {
+    "revision": "f44de37b43f145a052af",
+    "url": "/Robo/static/js/runtime-main.eeb555f1.js"
+  },
+  {
+    "revision": "da1fc23b1a2687b68fc00bd6e7fe9ced",
+    "url": "/Robo/static/media/AgeoPersonalUse-ExtraBold.da1fc23b.woff"
+  }
+]);
